b02a4934fbb6af06a71bdb654331dcfd8b4b730b
Lopun Lecture <lopun.lecture@gmail.com>
2024-07-23 11:58:44 +0900

    layout provider 컴포넌트 위치 수정 (html -> body only)

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,22 +17,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ReactQueryClientProvider>
-      <ThemeProvider>
-        <html lang="en">
-          <head>
-            <link
-              rel="stylesheet"
-              href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
-              integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
-              crossOrigin="anonymous"
-              referrerPolicy="no-referrer"
-            />
-          </head>
+    <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+          integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </head>
 
-          <body className={inter.className}>{children}</body>
-        </html>
-      </ThemeProvider>
-    </ReactQueryClientProvider>
+      <body className={inter.className}>
+        <ReactQueryClientProvider>
+          <ThemeProvider>{children}</ThemeProvider>
+        </ReactQueryClientProvider>
+      </body>
+    </html>
   );
 }
